2ab13e700d59a7ad0d4c94e42c1807c787a47373
MrBasut <138892868+MrBasut@users.noreply.github.com>
2025-09-22 18:58:18 +0300

    Update Get-4688procreate.ps1

--- Powershell/Event-Queries/Windows-Security/Get-4688procreate.ps1
+++ Powershell/Event-Queries/Windows-Security/Get-4688procreate.ps1
@@ -1,8 +1,15 @@
 
 
+$DaysBack= -1
+$HoursBack= -1
+$Process= 'powershell'
+
 
 Get-WinEvent -FilterHashtable @{
           LogName='Security';
           Id=4688;
-          StartTime=(Get-Date).AddDays()
-          }
+          StartTime=(Get-Date).AddDays($DaysBack)
+          } | Select-Object TimeCreated,
+                  Id,
+                  @{Name='ProcessName';Expression={($_.Message -match "New Process Name:\s+(\S+)") | Out-Null; $matches[1]}},
+                  @{Name='CreatorProcess';Expression={($_.Message -match "Creator Process Name:\s+(\S+)") | Out-Null; $matches[1]}}
