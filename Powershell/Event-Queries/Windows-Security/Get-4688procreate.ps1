ba9e77abd45c59a116a5ee3aac966874cd6da650
MrBasut <138892868+MrBasut@users.noreply.github.com>
2025-09-23 14:14:09 +0300

    Update Get-4688procreate.ps1

--- Powershell/Event-Queries/Windows-Security/Get-4688procreate.ps1
+++ Powershell/Event-Queries/Windows-Security/Get-4688procreate.ps1
@@ -2,6 +2,7 @@
 #Change the parameter DaysBack in the script with HoursBack if you are dealing with hours instead of days.
 #You may alter the values to suit your need.
 #You may change the comment block to filter for a specific process name and value of $Process.
+#Output format will be table with TimeCreated, CreatorAccount, TargetAccount, ProcessName and CreatorProcess
 
 $DaysBack= -1
 $HoursBack= -1
@@ -12,6 +13,8 @@ Get-WinEvent -FilterHashtable @{
           LogName='Security';
           Id=4688;
           StartTime=(Get-Date).AddDays($DaysBack)
-          } <#| Where-Object {$_Message -matches "Process Name=\s+$Process"#>| Select-Object TimeCreated,
+          } <#| Where-Object {$_.Message -match "New Process Name=\s+$Process"}#>| Select-Object TimeCreated,
+                  @{Name='CreatorAccount';Expression={($_.Message -match "Creator Subject:\s*[\s\S]*?Account Name:\s+(\S+)") | Out-Null; $matches[1]}},
+                  @{Name='TargetAccount';Expression={($_.Message -match "Target Subject:\s*[\s\S]*?Account Name:\s+(\S+)") | Out-Null; $matches[1]}},
                   @{Name='ProcessName';Expression={($_.Message -match "New Process Name:\s+(\S+)") | Out-Null; $matches[1]}},
                   @{Name='CreatorProcess';Expression={($_.Message -match "Creator Process Name:\s+(\S+)") | Out-Null; $matches[1]}}
